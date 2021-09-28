1028597338f4b1917696c77b794ec0192d3724d2
Timo Sneijder <89972625+defecon@users.noreply.github.com>
2021-09-28 12:19:45 +0200

    Add files via upload

---
+++ solicitatie.py.py
@@ -0,0 +1,43 @@
+if float (input ("Hoeveel jaar praktijkervaring heeft u met dieren-dressuur in totaal? :")) <= 4:
+    if float (input ("Hoeveel jaar ervaring heeft u met jongleren? : ")) <= 5:
+        if float (input("hoeveel jaar praktijkervaring heeft u met acrobatiek in totaal? :")) <= 3:
+            print("U voeldoet niet aan onze eisen")
+
+if input("Bent u in het bezit van een MBO4 diploma? (J/N) : ") != "J":
+    print("U voeldoet niet aan onze eisen")
+
+if input("Bent u in het bezit van een geldig vrachtwagen bewijs? (J/N) : ") != "J":
+    print("U voeldoet niet aan onze eisen")
+
+if input("Heeft u een hogehoed tot uw beschikking? (J/N) : ") != "J":
+    print("U voldoet niet aan de eis")    
+
+
+if input("Heeft u een jas tot uw beschikking? (J/N) : ") != "J":  
+    print("U voldoet niet aan de eis")
+
+if input("Heeft u schoenen tot uw beschikking? (J/N) : ") != "J":    
+    print("U voeldoet niet aan onze eisen")
+
+if input("bent u een man of een vrouw? MAN/VROUW : ") == "MAN":
+    if float(input("Hoe breed is uw snor? : ")) <= 10:
+        print("U voeldoet niet aan onze eisen")
+else:
+    if input("Heeft u orange/vuurtorenRood kleur haar? (J/N) ") != "J":
+        print("U voldoet niet aan onze eisen")
+        if float (input("Hoeveel cm is uw haar? : ")) <= 20:
+            print("u voldoet niet aan onze eisen")
+
+
+if float(input("Hoeveel cm lang bent u? : ")) <= 150:
+    print("U voldoet niet aan onze eis")
+
+if float(input("Hoeveel kg weegt u? : ")) <= 90:
+
+ if input("Heeft u een certificaat van overleven met gevaarlijk personeel? (J/N) : ") != "J":
+    print("u voldoet niet aan onze eis")
+
+
+
+
+print("He WTF je bent gewoon gekozen lekker maat, bel het nummer 0620476014")            
